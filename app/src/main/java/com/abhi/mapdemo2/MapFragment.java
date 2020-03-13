96ed2a0d2367a4745354d4af3d5ba767e55ea41b
AbhilashBasumatary <abhilashbasumatary@gmail.com>
2020-03-12 21:11:32 -0700

    Initial commit

---
+++ app/src/main/java/com/abhi/mapdemo2/MapFragment.java
@@ -0,0 +1,60 @@
+package com.abhi.mapdemo2;
+
+import android.app.VoiceInteractor;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng pp = new LatLng(11.5448729, 104.8921668);
+        MarkerOptions option = new MarkerOptions();
+        option.position(pp).title("Phnom Penh");
+        map.addMarker(option);
+        map.moveCamera(CameraUpdateFactory.newLatLng(pp));
+    }
+}
---
+++ app/src/main/java/com/abhi/mapdemo2/user.java
@@ -0,0 +1,14 @@
+package com.abhi.mapdemo2;
+
+public class user {
+    public String Username, Email;
+
+    public user(){
+
+    }
+
+    public user(String username, String email) {
+        Username = username;
+        Email = email;
+    }
+}
